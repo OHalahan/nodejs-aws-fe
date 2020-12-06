97034d4c37418c1a1b1ddef624d6b8b5e1bfa7fe
Oleksandr Halahan <Oleksandr_Halahan@epam.com>
2020-12-06 22:39:43 +0200

    Configure Cart API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,10 @@ const API_PATHS = {
   order: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://m4vi868ne8.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  // Below is Elasticbeanstalk URL but it cannot be used because it is served using HTTP
+  // cart: 'http://ohalahan-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
+  // use HTTPS proxy instead
+  cart: 'https://no8p3hmfgh.execute-api.eu-west-1.amazonaws.com/develop/api',
 };
 
 export default API_PATHS;
